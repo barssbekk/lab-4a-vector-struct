75d7cd8354fcd95e1ae577fe6b2d5526eb312a99
Barsbek Azamatov <bars.moderside@gmail.com>
2026-02-08 20:13:45 -0800

    Add struct Color

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 // COMSC-210 | Lab 4A | Barsbek
 #include <iostream>
 #include <vector>
+using namespace std;
+
+struct Color {
+    int red;
+    int green;
+    int blue;
+};
 
 int main() {
-    
+    Color test{};
+    test.blue = 51;
+    cout << test.blue;
     return 0;
 }
